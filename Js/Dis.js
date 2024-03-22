52a72332d5fcb88ef997f031055e5b55146c2abd
github_username <riofortin@gmail.com>
2024-03-22 16:57:59 -0600

    Click

--- Js/Dis.js
+++ Js/Dis.js
@@ -1,26 +1,26 @@
 
 document.getElementById("prgS").addEventListener(
-    'oneclick', function() {
+    'click', function() {
         document.getElementById("pr1").style.display = "none";
         document.getElementById("pr2").style.display = "block";
     }
 )
 
 document.getElementById("prgS2").addEventListener(
-    'oneclick', function() {
+    'click', function() {
         document.getElementById("pr2").style.display="none";
         document.getElementById("1").style.display = "block";
         document.getElementById("2").style.display = "block";
     }
 )
-document.getElementById("BVer").addEventListener('oneclick', function() {
+document.getElementById("BVer").addEventListener('click', function() {
     document.getElementById("resultado").style.display = "block";
     document.getElementById("1").style.display = "none";
     document.getElementById("2").style.display = "none";
 
 })
 
-document.getElementById("BotonCerrar").addEventListener('oneclick', function() {
+document.getElementById("BotonCerrar").addEventListener('click', function() {
     document.getElementById("resultado").style.display = "none";
     document.querySelector(".Contenedor-Binicio").style.display = "none";
     document.querySelector(".Con-2").style.display = "block";
