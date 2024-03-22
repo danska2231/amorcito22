cc35af3c47f611fe1055e640ad448df9903d1952
danska2231 <94161591+danska2231@users.noreply.github.com>
2024-03-22 16:12:04 -0600

    Add files via upload

---
+++ Js/Dis.js
@@ -0,0 +1,17 @@
+
+document.getElementById("prgS").addEventListener(
+    'touchstart', function() {
+        document.getElementById("pr1").style.display = "none";
+        document.getElementById("pr2").style.display = "block";
+    }
+)
+
+document.getElementById("BVer").addEventListener('click', function() {
+    document.getElementById("resultado").style.display = "block";
+})
+
+document.getElementById("BotonCerrar").addEventListener('click', function() {
+    document.getElementById("resultado").style.display = "none";
+    document.querySelector(".Contenedor-Binicio").style.display = "none";
+    document.querySelector(".Con-2").style.display = "block";
+})
