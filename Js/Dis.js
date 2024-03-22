f2e20078e9eccbe1fc72b78b062274c4d88ec38f
github_username <riofortin@gmail.com>
2024-03-22 16:48:22 -0600

    Probando

--- Js/Dis.js
+++ Js/Dis.js
@@ -1,16 +1,26 @@
 
 document.getElementById("prgS").addEventListener(
-    'oneclick', function() {
+    'touchstart', function() {
         document.getElementById("pr1").style.display = "none";
         document.getElementById("pr2").style.display = "block";
     }
 )
 
-document.getElementById("BVer").addEventListener('oneclick', function() {
+document.getElementById("prgS2").addEventListener(
+    'touchstart', function() {
+        document.getElementById("pr2").style.display="none";
+        document.getElementById("1").style.display = "block";
+        document.getElementById("2").style.display = "block";
+    }
+)
+document.getElementById("BVer").addEventListener('touchstart', function() {
     document.getElementById("resultado").style.display = "block";
+    document.getElementById("1").style.display = "none";
+    document.getElementById("2").style.display = "none";
+
 })
 
-document.getElementById("BotonCerrar").addEventListener('click', function() {
+document.getElementById("BotonCerrar").addEventListener('touchstart', function() {
     document.getElementById("resultado").style.display = "none";
     document.querySelector(".Contenedor-Binicio").style.display = "none";
     document.querySelector(".Con-2").style.display = "block";
